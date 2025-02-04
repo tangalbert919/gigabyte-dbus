7faad17a24e5bcef19d34c61dfed4f6eccb631f3
Albert Tang <tangalbert919@yahoo.com>
2025-02-04 11:15:48 -0800

    Implement get methods

--- src/platform.rs
+++ src/platform.rs
@@ -58,4 +58,58 @@ impl CtrlPlatform {
         device.set_attribute_value("fan_curve_data", &(value).to_string()).unwrap();
         0
     }
+    // get methods
+    fn get_fan_mode(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let fan_mode = device.attribute_value("fan_mode").unwrap();
+        fan_mode.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_fan_speed(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let fan_speed = device.attribute_value("fan_custom_speed").unwrap();
+        fan_speed.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_charge_mode(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let charge_mode = device.attribute_value("charge_mode").unwrap();
+        charge_mode.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_charge_limit(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let charge_limit = device.attribute_value("charge_limit").unwrap();
+        charge_limit.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_fan_curve_index(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let fan_curve_index = device.attribute_value("fan_curve_index").unwrap();
+        fan_curve_index.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_fan_curve_data(&self) -> (i32, i32) {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let fan_curve_data = device.attribute_value("fan_curve_data").unwrap();
+        let mut parts = fan_curve_data.to_str().unwrap().split_whitespace();
+        let temp = parts.next().unwrap().parse::<i32>().unwrap();
+        let speed = parts.next().unwrap().parse::<i32>().unwrap();
+        (temp, speed)
+    }
+    fn get_battery_cycle(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let battery_cycle = device.attribute_value("battery_cycle").unwrap();
+        battery_cycle.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_usb_s3_toggle(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let usb_s3_toggle = device.attribute_value("usb_charge_s3_toggle").unwrap();
+        usb_s3_toggle.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_usb_s4_toggle(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let usb_s4_toggle = device.attribute_value("usb_charge_s4_toggle").unwrap();
+        usb_s4_toggle.to_str().unwrap().parse::<i32>().unwrap()
+    }
+    fn get_gpu_boost(&self) -> i32 {
+        let device = Device::from_syspath(&self.path).unwrap();
+        let gpu_boost = device.attribute_value("gpu_boost").unwrap();
+        gpu_boost.to_str().unwrap().parse::<i32>().unwrap()
+    }
 }
