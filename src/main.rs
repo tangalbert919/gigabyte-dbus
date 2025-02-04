87c68325857744187948b10dc2cf0ddfaf70af8c
Albert Tang <tangalbert919@yahoo.com>
2025-02-04 11:09:44 -0800

    Add debug option
    This will allow testing without interfering with the real daemon.

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Some(val) => val == "1",
         None => false,
     };
+    let is_debug = match env::var_os("IS_DEBUG") {
+        Some(val) => val == "1",
+        None => false,
+    };
 
     if !is_service {
         println!("gigabyted should only be run as a systemd service!");
         println!("If you need logs, use journalctl -b -u gigabyted");
         println!("gigabyted will now exit");
         return Ok(());
+    }
+
+    if is_debug {
+        env::set_var("RUST_LOG", "debug");
+        println!("RUST_LOG set to debug");
+        println!("gigabyted will run on com.gigabyte.debug to avoid interfering with the real com.gigabyte.daemon");
     }
 
     let connection = Connection::session().await?;
@@ -33,7 +43,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    connection.request_name("com.gigabyte.daemon").await?;
+    // Request name
+    if is_debug {
+        connection.request_name("com.gigabyte.debug").await?;
+    } else {
+        connection.request_name("com.gigabyte.daemon").await?;
+    }
 
     // Do other things or go to wait forever
     pending::<()>().await;
